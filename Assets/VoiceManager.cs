8b72012893e9a6dd0293de87b6ff1cd653356bf0
HalaAlrifai <151993049+HalaAlrifai@users.noreply.github.com>
2026-07-17 13:39:51 +0200

    Resolve debugging issues

--- Assets/VoiceManager.cs
+++ Assets/VoiceManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 #if WINDOWS_UWP
 using Windows.Media.SpeechSynthesis;
@@ -9,15 +10,41 @@ using System;
 
 public class VoiceManager : MonoBehaviour
 {
+    //public TMP_Text debugText;
+
+    //private void ShowDebug(string message)
+    //{
+    //    debugText.text = "Hello";
+    //    if (debugText != null)
+    //    {
+    //        debugText.text = message;
+    //    }
+    //}
     public string phrase =
-        " Bonjour. Écoutez cette prononciation et indiquez celle que vous préférez.";
+        "Bonjour. Écoutez cette voix et choisissez celle que vous préférez.";
     public static VoiceManager Instance;
     private static bool isSpeaking = false;
     public string selectedVoice = "Hortense"; // Default voice
     public string selectedRate = "medium"; // Default rate
     public string selectedPitch = "medium"; // Default pitch
     public string selectedVolume = "100%"; // Default volume
+//    void Awake()
+//    {
+//        Instance = this;
 
+//        ShowDebug("VoiceManager Awake");
+
+//#if WINDOWS_UWP
+//    synthesizer = new SpeechSynthesizer();
+//    mediaPlayer = new MediaPlayer();
+
+//    mediaPlayer.MediaEnded += (sender, args) =>
+//    {
+//        isSpeaking = false;
+//        ShowDebug("Lecture terminée");
+//    };
+//#endif
+    //}
 
 #if WINDOWS_UWP
     private SpeechSynthesizer synthesizer;
@@ -28,20 +55,20 @@ public class VoiceManager : MonoBehaviour
     {
         Instance = this;
 #if WINDOWS_UWP
-        synthesizer = new SpeechSynthesizer();
-        mediaPlayer = new MediaPlayer();
+            synthesizer = new SpeechSynthesizer();
+            mediaPlayer = new MediaPlayer();
 
-        mediaPlayer.MediaEnded += (sender, args) =>
-        {
-            isSpeaking = false;
-        };
+            mediaPlayer.MediaEnded += (sender, args) =>
+            {
+                isSpeaking = false;
+            };
 #endif
     }
 
     // Voix féminine 1 : Hortense normale
     public void PlayVoice1()
     {
-       
+
 
 #if WINDOWS_UWP
         SetVoice("Hortense");
@@ -51,13 +78,13 @@ public class VoiceManager : MonoBehaviour
         selectedPitch = "medium";
         selectedVolume = "100%";
 
-        SpeakSelectedVoice(" Bonjour. Écoutez cette prononciation et indiquez celle que vous préférez.");
+        SpeakSelectedVoice("Bonjour. Écoutez cette voix et choisissez celle que vous préférez.");
     }
 
     // Voix féminine 2 : Hortense lente
     public void PlayVoice2()
     {
-      
+
 
 #if WINDOWS_UWP
         SetVoice("Hortense");
@@ -68,13 +95,13 @@ public class VoiceManager : MonoBehaviour
         selectedPitch = "medium";
         selectedVolume = "100%";
 
-        SpeakSelectedVoice(" Bonjour. Écoutez cette prononciation et indiquez celle que vous préférez.."); ;
+        SpeakSelectedVoice("Bonjour. Écoutez cette voix et choisissez celle que vous préférez."); ;
     }
 
     // Voix masculine 1 : Paul normal
     public void PlayVoice3()
     {
-        
+
 
 #if WINDOWS_UWP
         SetVoice("Paul");
@@ -85,13 +112,13 @@ public class VoiceManager : MonoBehaviour
         selectedPitch = "medium";
         selectedVolume = "100%";
 
-        SpeakSelectedVoice(" Bonjour. Écoutez cette prononciation et indiquez celle que vous préférez.");
+        SpeakSelectedVoice("Bonjour. Écoutez cette voix et choisissez celle que vous préférez.");
     }
 
     // Voix masculine 2 : Paul lent
     public void PlayVoice4()
     {
-        
+
 
 #if WINDOWS_UWP
         SetVoice("Paul");
@@ -102,7 +129,7 @@ public class VoiceManager : MonoBehaviour
         selectedPitch = "medium";
         selectedVolume = "100%";
 
-        SpeakSelectedVoice(" Bonjour. Écoutez cette prononciation et indiquez celle que vous préférez.");
+        SpeakSelectedVoice("Bonjour. Écoutez cette voix et choisissez celle que vous préférez.");
     }
 
     public void SpeakSelectedVoice(string phrase)
@@ -110,11 +137,27 @@ public class VoiceManager : MonoBehaviour
         if (isSpeaking) return;
 
 #if WINDOWS_UWP
-        SetVoice(selectedVoice);
+            SetVoice(selectedVoice);
 #endif
 
         Speak(phrase, selectedRate, selectedPitch, selectedVolume);
     }
+    //    public void SpeakSelectedVoice(string phrase)
+    //    {
+    //        ShowDebug("SpeakSelectedVoice appelée");
+
+    //        if (isSpeaking)
+    //        {
+    //            ShowDebug("Déjà en train de parler");
+    //            return;
+    //        }
+
+    //#if WINDOWS_UWP
+    //    SetVoice(selectedVoice);
+    //#endif
+
+    //        Speak(phrase, selectedRate, selectedPitch, selectedVolume);
+    //    }
 
 #if WINDOWS_UWP
     private void SetVoice(string voiceName)
@@ -128,6 +171,18 @@ public class VoiceManager : MonoBehaviour
             }
         }
     }
+//    private void SetVoice(string voiceName)
+//{
+//    foreach (var voice in SpeechSynthesizer.AllVoices)
+//    {
+//        if (voice.DisplayName.Contains(voiceName))
+//        {
+//            synthesizer.Voice = voice;
+//            ShowDebug("Voix : " + voice.DisplayName);
+//            break;
+//        }
+//    }
+//}
 
     private async void Speak(string phrase, string rate, string pitch, string volume)
     {
@@ -143,10 +198,33 @@ public class VoiceManager : MonoBehaviour
         mediaPlayer.Source = MediaSource.CreateFromStream(stream, stream.ContentType);
         mediaPlayer.Play();
     }
+//    private async void Speak(string phrase, string rate, string pitch, string volume)
+//{
+//    isSpeaking = true;
+
+//    ShowDebug("Début synthèse");
+
+//    string ssml =
+//        "<speak version='1.0' xml:lang='fr-FR'>" +
+//        "<prosody rate='" + rate + "' pitch='" + pitch + "' volume='" + volume + "'>" +
+//        phrase +
+//        "</prosody></speak>";
+
+//    var stream = await synthesizer.SynthesizeSsmlToStreamAsync(ssml);
+
+//    ShowDebug("Lecture...");
+
+//    mediaPlayer.Source = MediaSource.CreateFromStream(stream, stream.ContentType);
+//    mediaPlayer.Play();
+//}
 #else
     private void Speak(string phrase, string rate, string pitch, string volume)
     {
-        Debug.Log($"Phrase: {phrase} | Voice: {selectedVoice} | rate={rate}");
+    //    ShowDebug(
+    //    "Mode Unity Editor\n" +
+    //    "Phrase : " + phrase + "\n" +
+    //    "Voix UWP non disponible dans l'éditeur"
+    //);
     }
 #endif
 }
